d4fbef15adb2916b230d3f291c8d19f8836a0539
taehun.kim <xognswldk@gmail.com>
2025-09-22 13:31:15 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',   // 🔴 리포 이름과 동일하게!
 })
